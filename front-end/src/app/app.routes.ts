54f173993522a6d29899cb73aaf3500254fdd711
Julio Sanchez <dam1cloud22@gmail.com>
2026-04-10 10:35:32 +0200

    Front-Actualizado,3 pantallas correctas

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Track } from './track/track'; 
+import { AiCoach} from './ai-coach/ai-coach';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'track', component: Track },
+  { path: 'ai-coach', component: AiCoach},
+
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
