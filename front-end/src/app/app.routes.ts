90d566c489aa1fae816f2a08645cade3a3ed5aab
Julio Sanchez <dam1cloud22@gmail.com>
2026-04-10 10:21:29 +0200

    Montando front-end, inicio de sesion y track completado

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Track } from './track/track'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'track', component: Track },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
