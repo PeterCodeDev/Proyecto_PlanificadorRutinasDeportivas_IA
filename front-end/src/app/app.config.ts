79bd9f34f479fd33c513458fed404816ed9e566a
Adrianpeter@Practicas <adrianpeter277@gmail.com>
2026-04-13 16:34:26 +0200

    app-config-ts actualizado

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideBrowserGlobalErrorListeners } from '@angular/core'; // Asegúrate de que la ruta de importación sea la correcta
+import { provideMarkdown } from 'ngx-markdown';
 
 import { routes } from './app.routes';
 
@@ -11,6 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     // Añadimos el motor para que el servicio de la IA funcione
-    provideHttpClient()
+    provideHttpClient(),
+    provideMarkdown()
   ]
 };
